a5371289a909a863d4b655925773edc75da68d27
Terry Lee <taelee1@WKMZT17C4BDE.global.publicisgroupe.net>
2022-05-25 11:18:14 -0400

    revised iterator pattern to accomodate Map interface

--- src/com/gdw888/designpattern/iterator/DinnerMenu.java
+++ src/com/gdw888/designpattern/iterator/DinnerMenu.java
@@ -1,20 +1,18 @@
 package com.gdw888.designpattern.iterator;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class DinnerMenu implements Iterable<MenuItem>{
-    private List<MenuItem> dinnerMenus;
+    private Map<String,MenuItem> dinnerMenus;
 
     public DinnerMenu(){
-        dinnerMenus = new ArrayList<>();
-        dinnerMenus.add(new MenuItem("Lobster","Pricey Menu", 100.00));
-        dinnerMenus.add(new MenuItem("Steak","Pricey steak Menu", 50.00));
+        dinnerMenus = new HashMap();
+        dinnerMenus.put("Lobster", new MenuItem("Lobster","Pricey Menu", 100.00));
+        dinnerMenus.put("Steak", new MenuItem("Steak","Pricey steak Menu", 50.00));
     }
 
     @Override
     public Iterator<MenuItem> iterator() {
-        return dinnerMenus.iterator();
+        return dinnerMenus.values().iterator();
     }
 }
